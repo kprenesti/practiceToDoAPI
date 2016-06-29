273f0789a0fc2d44dc893a66761fffeb7a4b1b0d
Krista Prenesti <kprenesti@gmail.com>
2016-06-29 19:04:01 -0400

    added get request for todos/:id

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.get('/todos', function(req, res){
   res.json(todos);
 });
 
+app.get('/todos/:id', function(req, res){
+  var todoID = parseInt(req.params.id);
+  var matching;
+  todos.forEach(function(item){
+    if(item.id === todoID){
+      matching = item;
+    }
+  }); //end forEach
+
+  if(matching){
+    res.json(matching);
+  } else {
+    res.status(404).send();
+  }
+});
+
 app.listen(PORT, function(){
   console.log('Server started on port '+ PORT);
 });
