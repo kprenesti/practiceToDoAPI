ba70be9bd6cac2123b429387b28910506f9ba3fc
Krista Prenesti <kprenesti@gmail.com>
2016-06-30 17:52:06 -0400

    Added PUT handler

--- server.js
+++ server.js
@@ -47,12 +47,47 @@ app.delete('/todos/:id', function(req, res){
   var todoID = parseInt(req.params.id);
   var unwanted = _.findWhere(todos, {id: todoID});
   if(!unwanted){
-    res.status(404).send('No item with id '+ todoID +' found.');
+    return res.status(404).send('No item with id '+ todoID +' found.');
   }
   var cleanedTodos = _.without(todos, unwanted);
   console.log(cleanedTodos);
   res.json(cleanedTodos);
 });
+
+//Update a todo (PUT)
+app.put('/todos/:id', function(req, res){
+  //Establish variables
+  var body = _.pick(req.body, 'description', 'completed');
+  var todoID = parseInt(req.params.id);
+  var itemToUpdate = _.findWhere(todos, {id: todoID});
+  var validAtts = {};
+
+  //Check for matching ids
+  if(!itemToUpdate){
+    return res.status(404).send();
+  }
+
+  //Check to make sure data submitted is valid
+  if(body.hasOwnProperty('completed')&& _.isBoolean(body.completed)){
+    validAtts.completed = body.completed;
+  } else if (body.hasOwnProperty(body.completed)){
+    res.status(400).send('Bad request.  Try again.');
+  }
+
+  if(body.hasOwnProperty('description')&& _.isString(body.description) && body.description.trim().length > 0){
+    validAtts.description = body.description;
+  } else if (body.hasOwnProperty(body.description)){
+    res.status(400).send('Bad request.  Try again.');
+  }
+
+  //Update item
+  _.extend(itemToUpdate, validAtts);
+
+  //Send response data
+  res.json(itemToUpdate);
+
+}); //end put
+
 
 app.listen(PORT, function(){
   console.log('Server started on port '+ PORT);
