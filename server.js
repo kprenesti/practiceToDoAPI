f21dbfa6a1b6965a3c10c2ef0c68da7de6568a0d
Krista Prenesti <kprenesti@gmail.com>
2016-07-04 16:31:34 -0400

    Fixed Get querys issue

--- server.js
+++ server.js
@@ -20,16 +20,25 @@ app.post('/todos', function(req, res){
     res.json(body);
 });
 
+//====== QUERY ITEMS OR GET ALL ITEMS (GET) =========//
+app.get('/todos', function (req, res) {
+	var queryParams = req.query;
+	var filteredTodos = todos;
 
-//======== GET ALL TODOS (GET) ===========//
-app.get('/', function(req, res){
-  res.send('Todo API Root');
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		  filteredTodos = _.filter(filteredTodos, function (todo) {
+			     return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
+	res.json(filteredTodos);
 });
-
-app.get('/todos', function(req, res){
-  res.json(todos);
-});
-
 
 // ======== GET INDIVIDUAL ITEM (GET) ========//
 app.get('/todos/:id', function(req, res){
@@ -88,25 +97,6 @@ app.put('/todos/:id', function(req, res){
 
 }); //end put
 
-//====== QUERY ITEMS (GET) =========//
-app.get('/todos', function (req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
-	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		  return filteredTodos = _.filter(filteredTodos, function (todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
-	}
-
-	res.json(filteredTodos);
-});
 
 
 app.listen(PORT, function(){
